699b932b3d385694ce86364b668a458d87ab0b38
Alcol <yuhang3661175@qq.com>
2020-12-29 20:49:06 +0800

    up

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.styl'
+
+const Header:React.FC = () => {
+  return (
+    <div className='header_container'>
+      <div>11</div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const Index:React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Index
